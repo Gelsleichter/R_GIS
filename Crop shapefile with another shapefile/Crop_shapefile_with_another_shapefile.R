1f8d171d13e6ed49889f116dc391da8aed9e2b66
Yuri Andrei Gelsleichter <42816926+Gelsleichter@users.noreply.github.com>
2023-09-29 16:55:34 +0200

    Update Crop_shapefile_with_another_shapefile.R
    Updated using the "terra" package

--- Crop shapefile with another shapefile/Crop_shapefile_with_another_shapefile.R
+++ Crop shapefile with another shapefile/Crop_shapefile_with_another_shapefile.R
@@ -1,15 +1,36 @@
 #############################################################################
 ### Crop shapefile with another shapefile ::: https://rdrr.io/cran/raster/man/crop.html
+### With Raster package
 library(raster)
-# library(rgdal) # if require
-# library(rgeos) # if require
 ### load a shape from raster package
-luxembourg <- shapefile(system.file("external/lux.shp", package="raster"))
-plot(luxembourg) 
+luxembourg_shp <- shapefile(system.file("external/lux.shp", package="raster"))
+plot(luxembourg_shp) 
 ### draw a simple shape
 another_shape <- as(extent(6, 6.4, 49.75, 50), 'SpatialPolygons')
-plot(another_shape, add=, border="red", lwd=3)
+plot(another_shape, add=T, border="red", lwd=1)
 ### crop (SpatialPolygon) luxembourg with another_shape
-luxembourg_clip <- crop(luxembourg, another_shape) 
+luxembourg_clip <- crop(luxembourg_shp, another_shape) 
 plot(luxembourg_clip, add=T, col=4) ### plot on 
 plot(luxembourg_clip, add=F, col=3) ### just plot
+
+################ Crop shapefile with another shapefile With "terra"
+library(terra)
+library(raster)
+lux_shp <- vect(system.file("ex/lux.shp", package = "terra"))
+plot(lux_shp)
+another_shape <- as(extent(6, 6.4, 49.75, 50), 'SpatialPolygons')
+plot(another_shape, add=T, border="red", lwd=1)
+### Clip Luxembourg shapefile with another_shape
+lux_clip <- crop(lux_shp, another_shape) 
+plot(lux_clip, add=T, col=4) ### plot on 
+plot(lux_clip, add=F, col=3) ### just plot
+
+################ Crop Raster with shapefile With "terra"
+lux_rast <- rast(system.file("ex/elev.tif", package = "terra"))
+plot(lux_rast)
+another_shape <- as(extent(6, 6.4, 49.75, 50), 'SpatialPolygons')
+plot(another_shape, add=T, border="red", lwd=1)
+### Clip Luxembourg shapefile with another_shape
+lux_clip <- crop(lux_rast, another_shape)
+plot(lux_clip, add=T) ### plot on 
+plot(lux_clip, add=F) ### just plot
